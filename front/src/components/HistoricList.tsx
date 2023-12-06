b05f495c48442f64e4e45ebb176c63a2cf26f8c4
caiosantos.dev@outlook.com <seu@email.com>
2023-12-06 18:53:52 -0300

    finalizando front end

--- front/src/components/HistoricList.tsx
+++ front/src/components/HistoricList.tsx
@@ -12,7 +12,7 @@ export const HistoricList = ({listHistoric, idUser}: HistoricProps) => {
     return (
         <ul className="w-full h-[87vh] md:w-[45rem] flex flex-col gap-4 py-8 px-4 overflow-y-auto">
             {
-                listHistoric.length > 0 && listHistoric.map((account: any) => <CardAccount account={account} idUser={idUser} />)
+                listHistoric.length > 0 && listHistoric.map((account: any) => <CardAccount key={account.id} account={account} idUser={idUser} />)
             }
         </ul>
     )
